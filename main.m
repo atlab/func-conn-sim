3f281ef9d7da792a69787a45f8adc2b500c38598
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-02-22 23:51:18 -0600

    debugged correlation matrix computation

--- main.m
+++ main.m
@@ -12,24 +12,18 @@ ipos = 1;
 epos = 1;
 C = 0;  % covariances
 M = 0;  % means
-progress_bar = waitbar(0,'Computing covariance matrix...');
+progress_bar = waitbar(0, 'Computing covariance matrix...');
 for bin = 1:nbins
-    vector = zeros(ne+ni, 1);
-    while s.espikes(epos,1)<bin*dt
+    vector = zeros(ne+ni, 1)+eps;
+    while epos <= size(s.espikes, 1) && s.espikes(epos,1)<binwidth*bin 
         cellnumber = s.espikes(epos,2);
         vector(cellnumber) = vector(cellnumber)+1;
         epos = epos + 1;
-        if epos > size(s.espikes, 1)
-            break
-        end
     end
-    while s.ispikes(ipos,1)<bin*dt
+    while ipos <= size(s.ispikes, 1) && s.ispikes(ipos,1)<binwidth*bin
         cellnumber = s.ispikes(ipos,2) + ne;
         vector(cellnumber) = vector(cellnumber)+1;
         ipos = ipos + 1;
-        if ipos > size(s.ispikes, 1)
-            break
-        end
     end
     waitbar(bin/nbins)
     C = C + vector*vector'/nbins;
@@ -41,14 +35,14 @@ C = C - M*M';
 R = corrcov(C);
 
 subplot 211
-imagesc(R, [-1 1]*0.05)
+imagesc(R(1:10:end,1:10:end), [-1 1]*0.1)
 axis image
 colormap(doppler)
 colorbar
 subplot 212
 [i,j] = meshgrid(1:ne+ni, 1:ne+ni);
 hist(R(i<j),100);
-ylim([0 200])
+grid on
 
 
 % estimatate partial correlations using lv-glasso 
